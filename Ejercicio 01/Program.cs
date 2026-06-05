501d80c076134bf828582d4de247b8397a827595
Yeremi Tantaraico <yeremitantaraico@gmail.com>
2026-06-04 23:56:18 -0500

    Ejercicio 01

---
+++ Ejercicio 01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Crear un arreglo para guardar 3 nombres
+        string[] estudiantes = new string[3];
+
+        // Registrar nombres en el arreglo
+        for (int i = 0; i < estudiantes.Length; i++)
+        {
+            Console.Write($"Nombre del estudiante {i + 1}: ");
+            estudiantes[i] = Console.ReadLine();
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Estudiantes registrados:");
+        // Mostrar nombres guardados en el arreglo
+        for (int i = 0; i < estudiantes.Length; i++)
+        {
+            Console.Write($"Índice {i}: {estudiantes[i]}");
+            if (i < estudiantes.Length - 1) Console.Write(" | ");
+        }
+        Console.WriteLine();
+    }
+}
