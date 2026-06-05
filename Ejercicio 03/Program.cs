c565c15092b9cb03cfe7a30be86976da6d0ac8f3
Yeremi Tantaraico <yeremitantaraico@gmail.com>
2026-06-04 23:58:33 -0500

    Ejercicio 03

---
+++ Ejercicio 03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] codigos = new int[5];
+
+        // Primer recorrido: registrar los códigos en el arreglo
+        for (int i = 0; i < codigos.Length; i++)
+        {
+            Console.Write($"Código {i + 1}: ");
+            codigos[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.Write("Código a buscar: ");
+        int buscado = int.Parse(Console.ReadLine());
+
+        // Se inicia en -1 porque todavía no se ha encontrado el código
+        int posicion = -1;
+
+        // Segundo recorrido: buscar el código dentro del arreglo
+        for (int i = 0; i < codigos.Length; i++)
+        {
+            if (codigos[i] == buscado)
+            {
+                posicion = i;
+                break; // Opcional: detiene el bucle al encontrar el primero
+            }
+        }
+
+        if (posicion != -1)
+        {
+            Console.WriteLine($"Código encontrado en índice {posicion}");
+        }
+        else
+        {
+            Console.WriteLine("Código no encontrado");
+        }
+    }
+}
